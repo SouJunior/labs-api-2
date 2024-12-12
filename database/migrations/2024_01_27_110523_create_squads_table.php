47fe8dcda0a2e48f63c71c955124bf1896c5ad70
Djhonantan Parreira <djhonantanedu@gmail.com>
2024-12-12 18:26:02 -0300

    Adiciona validação para rodar migrations que já estão em produção apenas em ambiente dev

--- database/migrations/2024_01_27_110523_create_squads_table.php
+++ database/migrations/2024_01_27_110523_create_squads_table.php
@@ -11,19 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('squads', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->uuid('uuid')->unique();
-            $table->uuid('product_uuid')->nullable();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->integer('active')->nullable()->default(1);
-            $table->datetimes();
+        if (app()->isLocal()) {
+            Schema::create('squads', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->uuid('uuid')->unique();
+                $table->uuid('product_uuid')->nullable();
+                $table->string('name');
+                $table->text('description')->nullable();
+                $table->integer('active')->nullable()->default(1);
+                $table->datetimes();
 
-            $table->index('uuid');
+                $table->index('uuid');
 
-            $table->foreign('product_uuid')->references('uuid')->on('products')->onDelete('cascade');
-        });
+                $table->foreign('product_uuid')->references('uuid')->on('products')->onDelete('cascade');
+            });
+        }
     }
 
     /**
--- database/migrations/2024_01_27_125916_create_member_table.php
+++ database/migrations/2024_01_27_125916_create_member_table.php
@@ -11,17 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('members', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->uuid('uuid')->unique();
-            $table->string('name');
-            $table->string('role');
-            $table->integer('active')->nullable()->default(1);
-            $table->datetimes();
+        if (app()->isLocal()) {
+            Schema::create('members', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->uuid('uuid')->unique();
+                $table->string('name');
+                $table->string('role');
+                $table->integer('active')->nullable()->default(1);
+                $table->datetimes();
 
-            $table->uuid('squad_uuid');
-            $table->foreign('squad_uuid')->references('uuid')->on('squads');
-        });
+                $table->uuid('squad_uuid');
+                $table->foreign('squad_uuid')->references('uuid')->on('squads');
+            });
+        }
     }
 
     /**
