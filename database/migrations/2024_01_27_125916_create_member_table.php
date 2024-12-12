2a67d9ee1851e55eaa6509717492ce0ab024a8db
Djhonantan Parreira <djhonantanedu@gmail.com>
2024-12-12 19:03:19 -0300

    Adiciona indice na coluna de chave estrangeira 'squad_uuid'

--- database/migrations/2024_01_27_125916_create_member_table.php
+++ database/migrations/2024_01_27_125916_create_member_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,6 +20,7 @@ return new class extends Migration
                 $table->datetimes();
 
                 $table->uuid('squad_uuid');
+                $table->index('squad_uuid');
                 $table->foreign('squad_uuid')->references('uuid')->on('squads');
             });
         }
